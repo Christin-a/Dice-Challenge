978e64ab800552d8b25f8b21b15b94e984c887c4
Christina Armand <christina.m.armand@gmail.com>
2022-02-26 20:36:59 -0500

    die game v2 Angela edits

--- script.js
+++ script.js
@@ -1,65 +1,32 @@
 
 
-// var getRandomNumber = (min, max) => {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-//   };
 
-
-// var rollDice = () =>  getRandomNumber(1, 6);
-
-var randomNumber1 = () => Math.floor(Math.random() * 7);
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 dice1 = document.getElementById("dice1");
 
-dice1.value = randomNumber1();
+randomImgSrc = "images/dice" + randomNumber1 + ".png";
 
+dice1.setAttribute("src", randomImgSrc);
+  
 
-
-if (dice1.value === 1) {
-    dice1.setAttribute("src", "images/dice1.png");
-  } else if (dice1.value === 2) {
-    dice1.setAttribute("src", "images/dice2.png");
-  } else if (dice1.value === 3) {
-    dice1.setAttribute("src", "images/dice3.png");
-  } else if (dice1.value === 4) {
-    dice1.setAttribute("src", "images/dice4.png");
-  } else if (dice1.value === 5) {
-    dice1.setAttribute("src", "images/dice5.png");
-  } else if (dice1.value === 6) {
-    dice1.setAttribute("src", "images/dice6.png");
-};
- 
-
-
-var randomNumber2 = () => Math.floor(Math.random() * 7);
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 dice2 = document.getElementById("dice2");
 
-dice2.value = randomNumber2();
+randomImgSrc2 = "images/dice" + randomNumber2 + ".png";
 
+dice2.setAttribute("src", randomImgSrc2);
 
-if (dice2.value === 1) {
-    dice2.setAttribute("src", "images/dice1.png");
-  } else if (dice2.value === 2) {
-    dice2.setAttribute("src", "images/dice2.png");
-  } else if (dice2.value === 3) {
-    dice2.setAttribute("src", "images/dice3.png");
-  } else if (dice2.value === 4) {
-    dice2.setAttribute("src", "images/dice4.png");
-  } else if (dice2.value === 5) {
-    dice2.setAttribute("src", "images/dice5.png");
-  } else if (dice2.value === 6) {
-    dice2.setAttribute("src", "images/dice6.png");
-};
 
 var winner = document.querySelector("h1");
 
-if (dice1.value > dice2.value) {
+if (randomNumber1 > randomNumber2) {
     winner.innerHTML = "Player 1 Wins! 🚩"
-} else if (dice1.value < dice2.value) {
+} else if (randomNumber1 < randomNumber2) {
     winner.innerHTML = "Player 2 Wins! 🚩"
 
 }else {
     winner.innerHTML = "It's a tie!"
 
-}
+};
